447f2be1f09073dbba527fd77462b1d07ddff5ae
lynshi <lyndrive@gmail.com>
2017-09-23 10:35:43 -0400

    basic plot for prices

--- bitcoin_price_to_sql.py
+++ bitcoin_price_to_sql.py
@@ -9,14 +9,16 @@ username = 'ajaykumar'
 password = 'ILoveAjay!!!'
 cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tcp:mhacks.database.windows.net,1433;Database=mhacks;Uid=ajaykumar@mhacks;Pwd=ILoveAjay!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = cnxn.cursor()
-# cursor.execute("CREATE TABLE AnalyzedData ( Interval nvarchar(255), Sent REAL, NumRecords INT, AVGSent Real, FinalPrice REAL, Delta REAL )")
-# cursor.commit()
+cursor.execute("DROP TABLE AnalyzedData")
+cursor = cnxn.cursor()
+cursor.execute("CREATE TABLE AnalyzedData ( Interval int, Sent REAL, NumRecords INT, AVGSent Real, FinalPrice REAL, Delta REAL )")
+cursor.commit()
 with open(data_file) as json_data:
     json_data = json.load(json_data)
 
 for week_num in json_data:
     cursor = cnxn.cursor()
     cursor.execute("INSERT INTO AnalyzedData(Interval, FinalPrice, Delta) VALUES({interval},{finalprice},{delta})".format(
-        interval=week_num,finalprice=json_data[week_num]['Final Price'],
+        interval=int(week_num),finalprice=json_data[week_num]['Final Price'],
         delta=json_data[week_num]['Delta Price']))
     cursor.commit()
--- price_parser.py
+++ price_parser.py
@@ -2,6 +2,13 @@ import json
 import datetime
 
 
+def change_dashes(dashed_string):
+    first_dash_pos = dashed_string.find('-')
+    second_dash_pos = dashed_string[first_dash_pos + 1:].find('-')
+    return dashed_string[first_dash_pos + 1:second_dash_pos - first_dash_pos - 1] + '/' \
+           + dashed_string[first_dash_pos + second_dash_pos + 2:] + '/' + dashed_string[:first_dash_pos]
+
+
 def get_next_week(day):
     return day + datetime.timedelta(weeks=1)
 
@@ -18,8 +25,8 @@ next_date = get_next_week(start_date)
 while next_date < datetime.date.today():
     start_price = json_data['bpi'][str(start_date)]
     end_price = json_data['bpi'][str(next_date)]
-    weekly_data[str(start_date)] = {'Delta Price': end_price - start_price}
-    weekly_data[str(start_date)].update({'Final Price': end_price})
+    weekly_data[str(i)] = {'Delta Price': end_price - start_price}
+    weekly_data[str(i)].update({'Final Price': end_price})
     i = i + 1
     start_date = next_date
     next_date = get_next_week(start_date)
