d315b911e3e71c26697076025663615bae97e619
AppyFizz <bakshi.shreyan@gmail.com>
2017-09-23 13:44:36 -0400

    parser finished

--- Aylien.py
+++ Aylien.py
@@ -1,9 +1,13 @@
 import aylien_news_api
 import time
+import datetime
 from aylien_news_api.rest import ApiException
-from pprint import pprint
 
 def fetch_new_stories(params={}):
+    aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-ID'] = '8a0e6374'
+    aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-Key'] = 'a1ba885d7d7d4e160b16d9f5c1fd551a'
+    api_instance = aylien_news_api.DefaultApi()
+
     fetched_stories = []
     stories = None
     while stories is None or len(stories) > 0:
@@ -17,32 +21,75 @@ def fetch_new_stories(params={}):
         stories = response.stories
         params['cursor'] = response.next_page_cursor
         fetched_stories += stories
-        print("Fetched %d stories. Total story count so far: %d" %
-              (len(stories), len(fetched_stories)))
+        # print("Fetched %d stories. Total story count so far: %d" %
+        #       (len(stories), len(fetched_stories)))
     return fetched_stories
 
-# Configure API key authorization: app_id
-aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-ID'] = '8a0e6374'
-# Configure API key authorization: app_key
-aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-Key'] = 'a1ba885d7d7d4e160b16d9f5c1fd551a'
 
-# create an instance of the API class
-api_instance = aylien_news_api.DefaultApi()
+# MY CODE BEGINS HERE
 
-
-params = {
+opts = {
   'title' : 'bitcoin',
   'language': ['en'],
-  'published_at_start': 'NOW-5HOUR',
-  'published_at_end': 'NOW',
+  'published_at_start': '2015-11-22T00:00:00Z',
+  'published_at_end': '2015-11-29T00:00:00Z',
   'cursor': '*',
   'per_page': 100
 }
-count = 1
-stories = fetch_new_stories(params)
-print("\n\n")
 
-for story in stories:
-        print(story.id)
-        print(story.title)
-        print(story.body)
+startDate = datetime.date(2015, 11, 22)
+week = datetime.timedelta(7, 0, 0, 0, 0, 0)
+endDate = startDate + week
+finalDate = datetime.date(2017, 9, 24)
+
+day = datetime.timedelta(1, 0, 0, 0, 0, 0)
+
+def resetTimer():
+    global startDate
+    global week
+    global endDate
+    global finalDate
+    startDate = datetime.date(2015, 11, 22)
+    week = datetime.timedelta(7, 0, 0, 0, 0, 0)
+    endDate = startDate + week
+    finalDate = datetime.date(2016, 1, 1)
+
+def hasNext():
+    return endDate < finalDate
+
+def dtToUtc(dt):
+    return str(dt.year).zfill(4) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)
+
+def getNextWeek():
+    global opts
+    global startDate
+    global week
+    global endDate
+    articles = []
+    if endDate <= finalDate:
+        # del opts['published_at_start']
+        # del opts['published_at_end']
+        # opts['published_at_start'] = dtToUtc(startDate) + 'T00:00:00Z'
+        # opts['published_at_end'] = dtToUtc(endDate) + 'T00:00:00Z'
+        opts = {
+            'title': 'bitcoin',
+            'language': ['en'],
+            'published_at_start': dtToUtc(startDate) + 'T00:00:00Z',
+            'published_at_end': dtToUtc(endDate) + 'T00:00:00Z',
+            'cursor': '*',
+            'per_page': 100
+        }
+        stories = fetch_new_stories(opts)
+        for story in stories:
+            articles.append((dtToUtc(startDate), story.title + "\n\n" + story.body))
+    startDate = endDate + day
+    endDate = startDate + week
+    return articles
+
+
+resetTimer()
+while hasNext():
+    w = getNextWeek()
+    for i in w:
+        print(i)
+    print("\n\n")
