2e90ebec8d34bdcff21990ee7266f029badc4abe
lynshi <lyndrive@gmail.com>
2017-09-23 09:13:16 -0400

    update with price data

--- bitcoin_price_to_sql.py
+++ bitcoin_price_to_sql.py
@@ -1,4 +1,23 @@
 import pyodbc
+import json
 
-cnxn = pyodbc.connect('Server=tcp:mhacks.database.windows.net,1433;Initial Catalog=MHacks;Persist Security Info=False;User ID=ajaykumar;Password=ILoveAjay!!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;')
+data_file = 'data/bitcoin_weekly_data.json'
+
+server = 'tcp:mhacks.database.windows.net'
+database = 'MHacks'
+username = 'ajaykumar'
+password = 'ILoveAjay!!!'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
 cursor = cnxn.cursor()
+cursor.execute("CREATE TABLE Bitcoin_Price_Data ( Interval INTEGER, FinalPrice REAL, Delta REAL )")
+cursor.commit()
+with open(data_file) as json_data:
+    json_data = json.load(json_data)
+
+for week_num in json_data:
+    cursor = cnxn.cursor()
+    cursor.execute("INSERT INTO Bitcoin_Price_Data VALUES({interval},{finalprice},{delta})".format(
+        interval=int(week_num),finalprice=json_data[week_num]['Final Price'],
+        delta=json_data[week_num]['Delta Price']))
+    cursor.commit()
