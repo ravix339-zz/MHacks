388bd4b25f4a4cb935634b902739b807305f0bf5
Ravi Sinha <ravix339@gmail.com>
2017-10-05 22:23:12 -0400

    Update SQL_Interface.py

--- SQL_Interface.py
+++ SQL_Interface.py
@@ -10,29 +10,6 @@ def Execute(query):
     cursor = connection.cursor()
     cursor.execute(query)
     cursor.commit()
-# def LyndonShiAbandonedUs():
-#     files = glob.glob("D:\\Code\\Python\\Text\\*.txt")
-#     for fileName in files:
-#         print(fileName)
-#         articles =[]
-#         with open(fileName,'r', encoding='utf-8', errors='ignore') as fileRead:
-#             try:
-#                 articles = fileRead.read().split('\r\n\r\n\r\n\r\n\r\n')
-#             except Exception as e:
-#                 print(e)
-#         for article in articles:
-#             global prev_file_name, score_list, mag_list
-#             sentScore = GS.getSentiment(text= article[1:-1])
-#             score_list.append(float(sentScore[0]))
-#             mag_list.append(float(sentScore[1]))
-#             if fileName[:11] != prev_file_name:
-#                 cursor = connection.cursor()
-#                 cursor.execute("UPDATE AnalyzedData SET Score = {score}, Mag = {mag} WHERE startdate = {startdate}".format(
-#                     startdate="".join(fileName.split('\\')[-1][:11].split('-')), score=np.mean(score_list), mag=np.mean(mag_list)))
-#                 cursor.commit()
-#                 score_list = []
-#                 mag_list = []
-#             prev_file_name = fileName[:11]
 
 def LyndonShiAbandonedUs():
     files = glob.glob("D:\\Code\\Python\\Text\\*.txt")
